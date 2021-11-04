69d537618522fa3c887c6018d3a7edb738ce9f77
Neusia Hilario <neusiahilariomassingue@gmail.com>
2021-11-04 18:24:19 +0200

    modifiquei a linguagem da data-table

--- public/js/data-table/data-table-act.js
+++ public/js/data-table/data-table-act.js
@@ -1,11 +1,33 @@
 (function ($) {
  "use strict";
-	
+
+
 	$(document).ready(function() {
+
 		 $('#data-table-basic').DataTable({
+			
+
 			 language:{
-				 searchPlaceholder: "Clique para pesquisar..."
-			 }
+				 
+			
+				 "lengthMenu": "Apresentar _MENU_ linhas por página.",
+				 "zeroRecords": "Não há registos",
+				 "search": "Pesquisar:",
+				 "info": "Mostrando a página _PAGE_ de _PAGES_ ",
+				 searchPlaceholder: "Clique para pesquisar...",
+			   "infoEmpty": " Não há registos",	
+			"infoFiltered": "(Filtrando de _MAX_ registos)",
+		"paginate": {
+			"first":   "primeiro",
+			"last":    "último",
+			"next":    "Seguinte",
+			"previous":  "Anterior"
+
+		
+		}		 },
+			 
+			 
+
 		 });
 	});
  
