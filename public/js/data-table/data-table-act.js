0292da2d015e6e96525905bcb6d9a1d25a82922f
Neusia Hilario <neusiahilariomassingue@gmail.com>
2021-11-08 19:00:14 +0200

    modifiquei o data-table-act.js

--- public/js/data-table/data-table-act.js
+++ public/js/data-table/data-table-act.js
@@ -1,34 +1,35 @@
 (function ($) {
- "use strict";
-
-
-	$(document).ready(function() {
-
-		 $('#data-table-basic').DataTable({
-			
-
-			 language:{
-				 
-			
-				 "lengthMenu": "Apresentar _MENU_ linhas por página.",
-				 "zeroRecords": "Não há registos",
-				 "search": "Pesquisar:",
-				 "info": "Mostrando a página _PAGE_ de _PAGES_ ",
-				 searchPlaceholder: "Clique para pesquisar...",
-			   "infoEmpty": " Não há registos",	
-			"infoFiltered": "(Filtrando de _MAX_ registos)",
-		"paginate": {
-			"first":   "primeiro",
-			"last":    "último",
-			"next":    "Seguinte",
-			"previous":  "Anterior"
-
-		
-		}		 },
-			 
-			 
-
-		 });
-	});
- 
-})(jQuery); 
+	"use strict";
+   
+   
+	   $(document).ready(function() {
+   
+			$('#data-table-basic').DataTable({
+			   
+			   "bFilter": false,
+   
+				language:{
+					
+			   
+					"lengthMenu": "Apresentar _MENU_ linhas por página.",
+					"zeroRecords": "Não há registos",
+					"search": "Pesquisar:",
+					"info": "Mostrando a página _PAGE_ de _PAGES_ ",
+					
+				  "infoEmpty": " Não há registos",	
+			   "infoFiltered": "(Filtrando de _MAX_ registos)",
+		   "paginate": {
+			   "first":   "primeiro",
+			   "last":    "último",
+			   "next":    "Seguinte",
+			   "previous":  "Anterior"
+   
+		   
+		   }		 },
+				
+				
+   
+			});
+	   });
+	
+   })(jQuery); 
